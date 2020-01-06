243319b5e79a69b3fed86b774587b95031a2a6d1
ItsNaSa <naphadesahil@gmail.com>
2020-01-06 14:57:59 +0530

    No addition

---
+++ weatherDataMapReduce/src/package1/MaxTemperatureMapper.java
@@ -0,0 +1,16 @@
+package package1;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+ 
+public class MaxTemperatureMapper {
+
+}
