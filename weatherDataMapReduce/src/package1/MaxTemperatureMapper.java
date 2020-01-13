1018bc46f776285fb618ebe4f4b393b60f441ef1
ItsNaSa <naphadesahil@gmail.com>
2020-01-13 14:42:43 +0530

    Adding asssignment 2

--- weatherDataMapReduce/src/package1/MaxTemperatureMapper.java
+++ weatherDataMapReduce/src/package1/MaxTemperatureMapper.java
@@ -1,16 +1,29 @@
 package package1;
 
 import java.io.IOException;
+
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
- 
-public class MaxTemperatureMapper {
 
+public class MaxTemperatureMapper extends Mapper<LongWritable,Text,Text,IntWritable>{
+    private static final int MISSING = 9999;
+    @Override
+    protected void map(LongWritable key, Text value, Mapper<LongWritable, Text, Text, IntWritable>.Context context)
+            throws IOException, InterruptedException {
+        String line = value.toString();
+        String year = line.substring(15,19);
+        int airTemperature;
+        if(line.charAt(87)=='+'){
+            airTemperature = Integer.parseInt(line.substring(88, 92));
+        }
+        else{
+            airTemperature = Integer.parseInt(line.substring(87, 92));
+        }
+        String quality = line.substring(92, 93);
+        if(airTemperature != MISSING && quality.matches("[01459]")){
+            context.write(new Text(year), new IntWritable(airTemperature));
+        }
+    }
 }
