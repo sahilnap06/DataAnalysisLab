8078e13c831da9e18848fef93473a4508ecf7dd6
Sahil Naphade <38909890+ItsNaSa@users.noreply.github.com>
2020-04-19 22:50:53 +0530

    Mergee commit

--- Assignment5/Assignment5_1.r
+++ Assignment5/Assignment5_1.r
@@ -1,41 +1,52 @@
-wd = getwd()
-wd
-setwd(wd)
+# Roll no. 33140
+# Batch L9
+# Assignment 5 (1) : Perform subsetting, melting and casting of dataset (Facebook).
 
-install.packages("readxl")
+setwd("G:/College/SL6/Assignment5/")
+
+# install.packages("readxl")
 library("readxl")
 
-dataset_Facebook = read.csv2("../../Sl-VI DataSets/dataset_Facebook.csv",header = T, sep = ';')
+dataset_Facebook = read.csv2("G:/College/Sl-VI DataSets/dataset_Facebook.csv",header = T, sep = ';')
 View(dataset_Facebook)
+dataset_Facebook
 
 #subsetting
 v1 <- dataset_Facebook[c(10:21),c(2,6:7)]
-View(v1)
+# View(v1)
+v1
 
+# subsetting as per comments
 v2 <- subset(dataset_Facebook,comment>15)
-View(v2)
+# View(v2)
+v2
 
+# Subset on different columns
 v3 <- dataset_Facebook[c(10:25),c(2,7:9)]
 View(v3)
+v3
 
 #merge data
 v1 <- dataset_Facebook[c(10:12),c(2,6:7)]
 v3 <- dataset_Facebook[c(11:14),c(2,7:9)]
 m1 <- merge(v1,v3,by="Paid")
-View(m1)
+# View(m1)
+m1
 
 #sort data
 s1 <- dataset_Facebook[order(dataset_Facebook$comment),]
-View(s1)
+# View(s1)
+s1
 
+#sorting data in ascending order as number of comments, and then decreasing number of shares
 s2 <- dataset_Facebook[order(dataset_Facebook$comment, -dataset_Facebook$share),]
-View(s2)
+# View(s2)
+s2
 
 # Transposing Data (Row names bacome column names)
 t(v1)
 
 # Melting Data to long format
-# https://www.statmethods.net/management/reshape.html
 library(reshape2)
 # Melt, i.e. each row is a unique ID-variable combination
 m1 <- melt(v3, id <-  c("Type","Lifetime.Post.Total.Reach"))
@@ -43,9 +54,8 @@ m2 <- m1 # Extra variable
 View(m1)
 head(m1)
 tail(m1)
+m1
 
-# http://www.cookbook-r.com/Manipulating_data/Converting_data_between_wide_and_long_format/
-# https://www.datacamp.com/community/tutorials/long-wide-data-R
 levels(m1$variable)[levels(m1$variable) == 'Paid'] <- 'first'
 levels(m1$variable)[levels(m1$variable) == 'Lifetime.Post.Total.Impressions'] <- 'second'
 m1
@@ -56,7 +66,7 @@ m1 <- m1[ order(m1$Type,m1$variable),]
 m1
 
 # Long to wide
-m2
 m2_wide = dcast(m2,Type + Lifetime.Post.Total.Reach ~ variable, value.var = "value")
   # This is the wide format data
 m2_wide
+
--- Assignment5/Assignment5_2.r
+++ Assignment5/Assignment5_2.r
@@ -9,6 +9,7 @@ data
 data$Quantity <- replace(data$Quantity,data$Quantity < 0,0)
 # COnsider a subset
 
+# subset 1 is of 1000 rows, with col no. 1 2 4 and 6
 subset1 <- data[c(1:1000),c(1,2,4,6)]
 subset1
 
@@ -19,12 +20,18 @@ t(subset1)
 
 library(reshape2)
 
+# sort the dataframe as per Customer ID
+data <- data[order(data$CustomerID),]
+data  # inspect the data
 
+# Melt the subset 1 in long format as per Invoice n quantity
 melt1 <- melt(subset1, id <- c("InvoiceNo","Quantity"))
 melt1
 
+# sort the melted data as per quantity and then invoice
 melt1 <- melt1[order(melt1$Quantity,melt1$InvoiceNo),]
 melt1
 
+# cast into Wide format
 melt2 <- dcast(melt1,Quantity + InvoiceNo ~ variable,value.var = "value")
 melt2
